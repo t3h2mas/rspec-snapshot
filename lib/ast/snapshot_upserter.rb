bfbcaf008159d9e2c16368ea78e16d3df785bc91
Thomas Noe <2mas@hey.com>
2023-01-17 23:26:48 -0700

    Linting

--- lib/ast/snapshot_upserter.rb
+++ lib/ast/snapshot_upserter.rb
@@ -12,18 +12,17 @@ module AST
       (send _ :match_inline_snapshot ...)
     PATTERN
 
-    def initialize(rewriter, start_loc, val)
+    def initialize(rewriter, start_loc, val) # rubocop:disable Lint/MissingSuper
       @rewriter = rewriter
       @start_loc = start_loc
       @val = val
     end
 
     def on_send(node)
-      match_inline_snapshot(node) do 
+      match_inline_snapshot(node) do
         next unless node.loc.expression.begin_pos == @start_loc
 
         @rewriter.replace(node.loc.expression, @val)
-        # p node.loc
       end
     end
   end
